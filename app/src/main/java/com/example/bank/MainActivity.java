eed9101aebf78a5bc3d875fa18b107efe679dadf
Bryan Huang <bryanleehuang1@gmail.com>
2021-05-10 16:02:04 -0700

    fixed sign up bugs

--- app/src/main/java/com/example/bank/MainActivity.java
+++ app/src/main/java/com/example/bank/MainActivity.java
@@ -118,45 +118,51 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                try {
+                String userName = etUserName.getText().toString();
+                String password = etPassword.getText().toString();
+                Double initialBalance = null;
+                if(!etInitialBalance.getText().toString().equals("")) initialBalance = Double.parseDouble(etInitialBalance.getText().toString());
 
-                    String userName = etUserName.getText().toString();
-                    String password = etPassword.getText().toString();
-                    Double initialBalance = Double.parseDouble(etInitialBalance.getText().toString());
 
-                    if (userName == null || password == null || initialBalance == null) {
-                        throw new InvalidInputException();
-                    }
-
-                    if (!userName.matches("[_\\-\\.0-9a-z]{1,127}")
-                            || !password.matches("[_\\-\\.0-9a-z]{1,127}")) {
-                        throw new InvalidInputException();
-                    }
-
-                    if (etInitialBalance.getText().toString().startsWith("0") && initialBalance >= 1) {
-                        throw new InvalidInputException();
-                    }
-
-                    if (etInitialBalance.getText().toString().contains(".")
-                            && (etInitialBalance.getText().toString().length() - etInitialBalance.getText().toString().indexOf('.') - 1) > 2) {
-                        throw new InvalidInputException();
-                    }
-                    else {
-                        User userCheck = db.userDao().selectSingleUserByName(userName);
-
-                        if (userCheck == null) {
-                            User user = new User(userName, password);
-                            db.userDao().insertSingleUser(user);
-                            user.setBalance(initialBalance);
-                            db.userDao().updateSingleUser(user);
-                            Toast.makeText(MainActivity.this, "Sign Up Successfully", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(MainActivity.this, "User already exists", Toast.LENGTH_SHORT).show();
-                        }
-
-                    }
+                if (userName == null || password == null || initialBalance == null) {
+                    txtMessage.setText("All fields must be complete for signup.");
+                    txtMessage.setTextColor(getResources().getColor(R.color.negativeRed));
+                    txtMessage.setVisibility(View.VISIBLE);
                 }
-                catch (InvalidInputException e){
+
+                else if (!userName.matches("[_\\-\\.0-9a-z]{1,127}")
+                        || !password.matches("[_\\-\\.0-9a-z]{1,127}")) {
+                    txtMessage.setText("Your user name and password must be between 1 - 127 characters long and could only include underscores, hyphens, dots, digits, and lowercase alphabetical characters.");
+                    txtMessage.setTextColor(getResources().getColor(R.color.negativeRed));
+                    txtMessage.setVisibility(View.VISIBLE);
+                }
+
+                else if (etInitialBalance.getText().toString().startsWith("0") && initialBalance >= 1) {
+                }
+
+                else if (etInitialBalance.getText().toString().contains(".")
+                        && (etInitialBalance.getText().toString().length() - etInitialBalance.getText().toString().indexOf('.') - 1) > 2) {
+                    txtMessage.setText("The initial balance can only have a maximum of 2 decimal points.");
+                    txtMessage.setTextColor(getResources().getColor(R.color.negativeRed));
+                    txtMessage.setVisibility(View.VISIBLE);
+                    return;
+                }
+                else {
+                    User userCheck = db.userDao().selectSingleUserByName(userName);
+
+                    if (userCheck == null) {
+                        User user = new User(userName, password);
+                        db.userDao().insertSingleUser(user);
+                        user.setBalance(initialBalance);
+                        db.userDao().updateSingleUser(user);
+                        txtMessage.setText("Sign up successful!");
+                        txtMessage.setTextColor(getResources().getColor(R.color.positiveGreen));
+                        txtMessage.setVisibility(View.VISIBLE);
+                    } else {
+                        txtMessage.setText("User already exists.");
+                        txtMessage.setTextColor(getResources().getColor(R.color.negativeRed));
+                        txtMessage.setVisibility(View.VISIBLE);
+                    }
 
                 }
             }
